b0c2723a8ace404827b53a90a1887669c935aea6
vsantiagos <vsantiagos>
2016-08-03 15:01:55 -0500

    Insertion Sort

---
+++ Insertion-Sort/Insertion-Sort/InsertionSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort
+{
+    class InsertionSort<T>
+    {
+        /// <summary>
+        /// Define la manera de ordenar el
+        /// arreglo.
+        /// </summary>
+        private IComparer<T> _comparator;
+
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="comparator">Define la manera de ordenar el arreglo</param>
+        public InsertionSort(IComparer<T> comparator)
+        {
+            this._comparator = comparator;
+        }
+
+        /// <summary>
+        /// Ordena un arreglo ingresado
+        /// </summary>
+        /// <param name="originalArray">Arreglo a ordenar</param>
+        /// <returns>Arreglo ordenado</returns>
+        public T[] Sort(T[] originalArray)
+        {
+            //Crea una copia del arreglo original para no
+            //alterarlo.
+            T[] sortedArray = (T[]) originalArray.Clone();
+
+            //Recorre desde el segundo hasta el último elemento del arreglo
+            for (int i = 1; i < sortedArray.Length; i++)
+            {
+                //Elemento actual del arreglo
+                T key = sortedArray[i];
+                int j = i - 1;
+
+                //Mientras que los elementos anteriores al elemento actual esten
+                //desordenados...
+                while (j >= 0 && this._comparator.Compare(key, sortedArray[j]) < 0)
+                {
+                    sortedArray[j + 1] = sortedArray[j];
+                    j = j - 1;
+                }
+                //Acomoda el elemento actual en el lugar que le corresponde.
+                sortedArray[j + 1] = key;
+            }
+            return sortedArray;
+        }
+    }
+}
---
+++ Insertion-Sort/Insertion-Sort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertionSort<int> sorter = new InsertionSort<int>(new NonDecreasingIntegerComparer());
+            int[] originalArray = { 27, 32, 1, 6, 95, 3 };
+
+            Console.WriteLine("Original Array: {0}", IntegerArrayToString(originalArray));
+            int[] sortedArray = sorter.Sort(originalArray);
+            Console.WriteLine("Non Decreasing Sorted Array: {0}", IntegerArrayToString(sortedArray));
+        }
+
+        static String IntegerArrayToString(int[] array)
+        {
+            StringBuilder stringBuilder = new StringBuilder("[");
+
+            foreach (int i in array) 
+            {
+                stringBuilder.AppendFormat("{0}, ", i);
+            }
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            stringBuilder.Append("]");
+            return stringBuilder.ToString();
+        }
+    }
+
+    class NonDecreasingIntegerComparer : IComparer<int>
+    {
+        /// <summary>
+        /// Compara dos enteros ingresados
+        /// </summary>
+        /// <param name="x">Primer entero</param>
+        /// <param name="y">Segundo entero</param>
+        /// <returns>Regresa 0 si ambos enteros son iguales; 1 si x es mayor a y; -1 si x es menor a y</returns>
+        public int Compare(int x, int y)
+        {
+            return x == y ? 0 : x > y ? 1 : -1;
+        }
+    }
+}
